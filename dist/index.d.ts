4da813f976ef4dd0e530d7908261d8c6d42f7f08
Ilian Aleixev <akrion@gmail.com>
2022-06-24 13:32:44 -0500

    Add autocomplete

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
 declare const relLanguage: LRLanguage;
+declare const relCompletion: import("@codemirror/state").Extension;
 declare function rel(): LanguageSupport;
-export { relLanguage, rel };
+export { relLanguage, relCompletion, rel };
