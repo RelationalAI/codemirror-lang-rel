bdf78d33d1ca2384e286e85e065797077292e3dc
Osama Alfakhouri <osama.alfakhouri@relational.ai>
2022-10-17 13:37:47 +0300

    chore: change terminals highlighting tags

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {parser} from "./syntax.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import {styleTags, tags} from "@lezer/highlight"
 import {completeFromList, completionKeymap} from "@codemirror/autocomplete"
 import {autocompleteList} from './autocompleteList'
 
@@ -14,38 +14,38 @@ export const relLanguage = LRLanguage.define({
       //   Application: foldInside
       // }),
       styleTags({
-        'use forall for in iff if then else end where with select implies': t.controlKeyword,
-        'not and or xor': t.operatorKeyword,
-        'as from inline function ic doc raw entity type value': t.keyword,
-        'output insert delete abort export': t.emphasis,
-        'def': t.definitionKeyword,
-        'module': t.moduleKeyword,
-        'Any String Int Number Char Missing Float Floating UnsignedInt SignedInt Rational FixedDecimal RelName Entity AutoNumber Hash FilePos Date DateTime Year Month Week Day Hour Minute Second Millisecond Microsecond Nanosecond Boolean':
-          t.typeName,
-        Number: t.number,
-        BooleanLiteral: t.bool,
-        StringLiteral: t.string,
-        MultilineStringLiteral: t.string,
-        MultilineRawStringLiteral: t.string,
-        AssignOp: t.definitionOperator,
-        LineComment: t.lineComment,
-        BlockComment: t.blockComment,
-        MultilineBlockComment: t.blockComment,
-        VariableName: t.definition(t.variableName),
-        Atom: t.atom,
-        RelnameLiteral: t.literal,
-        RelnameStringLiteral: t.literal,
-        ValueTypeLiteral: t.literal,
-        DefineStatement: t.atom,
-        DateLiteral: t.string,
-        DateTimeLiteral: t.string,
-        Operator: t.operatorKeyword,
-        '( )': t.paren,
-        '[ ]': t.squareBracket,
-        '{ }': t.brace,
-        ':': t.derefOperator,
-        ', ;': t.separator
-      })
+        Keyword: tags.keyword,
+        AttributeKeyword: tags.keyword,
+        Type: tags.typeName,
+        Emphasis: tags.emphasis,
+        IntLiteral: tags.number,
+        FloatLiteral: tags.number,
+        BooleanLiteral: tags.bool,
+        DocstringLiteral: tags.string,
+        MultilineDocstringLiteral: tags.string,
+        "StaticStringLiteral/...": tags.string,
+        "StaticStringLiteral/StaticSequence/EscapeChar": tags.escape,
+        StaticMultilineStringLiteral: tags.string,
+        LineComment: tags.lineComment,
+        BlockComment: tags.blockComment,
+        BasicId: tags.definition(tags.variableName),
+        "LhsId/BasicId": tags.definition(tags.variableName),
+        "LogicalParam/LogicalExpression/BasicExpression/BasicId": tags.definition(tags.attributeName),
+        "BasicExpression/BasicId": tags.definition(tags.labelName),
+        "FormalId/BasicId": tags.definition(tags.attributeName),
+        "QualifiedNameElem/BasicId RelnameLiteral": tags.literal,
+        ValueTypeLiteral: tags.literal,
+        DefineStatement: tags.atom,
+        DateLiteral: tags.string,
+        DateTimeLiteral: tags.string,
+        Operator: tags.operatorKeyword,
+        OperatorKeyword: tags.operatorKeyword,
+        '( )': tags.paren,
+        '[ ]': tags.squareBracket,
+        '{ }': tags.brace,
+        ':': tags.derefOperator,
+        ', ;': tags.separator
+      }),
     ]
   }),
   languageData: {
